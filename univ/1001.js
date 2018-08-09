3a1d79be26289cd0e0dcfd8fae173482a910e7c8
Jsthon <jsthon@outlook.com>
2018-08-09 15:12:59 +0800

    add tsinghua university

---
+++ univ/1001.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "清华大学",
+    id: "1001",
+    link: [ {
+        name: "清华主页",
+        url: "https://www.tsinghua.edu.cn/"
+    }, {
+        name: "信息系统",
+        url: "http://its.tsinghua.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://lib.tsinghua.edu.cn/dra/"
+    }, {
+        name: "清华邮箱",
+        url: "http://mails.tsinghua.edu.cn/"
+    }, {
+        name: "SSLVPN",
+        url: "https://sslvpn.tsinghua.edu.cn"
+    }, {
+        name: "网络课堂",
+        url: "http://learn.tsinghua.edu.cn/"
+    }, {
+        name: "清华园",
+        url: "http://www.thubbs.cn/"
+    } ]
+};
