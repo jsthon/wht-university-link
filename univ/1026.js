16e0e05959009710fb48f13748169d62259c891c
ringsaturn <ringsaturn.me@gmail.com>
2018-08-11 17:11:24 +0800

    Create 1026.js

---
+++ univ/1026.js
@@ -0,0 +1,14 @@
+var univ_list = {
+    school: "北方工业大学",
+    id: "1026",
+    link: [{
+        name: "NCUT 主页",
+        url: "http://www.ncut.edu.cn/"
+    }, {
+        name: "Web VPN",
+        url: "https://webvpn.ncut.edu.cn/users/sign_in"
+    }, {
+        name: "图书馆",
+        url: "http://lib.ncut.edu.cn/lib/Index.html"
+    }]
+};
