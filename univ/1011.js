95265438227085b968ba43dc9bc29e6215cc03e9
spencerwoo98 <spencerwoo98@foxmail.com>
2018-08-11 16:17:14 +0800

    添加北理链接 | Add BIT Links

---
+++ univ/1011.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "北京理工大学",
+    id: "1011",
+    link: [ {
+        name: "北理主页",
+        url: "https://www.bit.edu.cn/"
+    }, {
+        name: "教务处",
+        url: "http://jwc.bit.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://lib.bit.edu.cn/"
+    }, {
+        name: "北理在线 | 综合信息服务平台",
+        url: "http://online.bit.edu.cn"
+    }, {
+        name: "乐学 | 网络教室",
+        url: "http://online.bit.edu.cn/moodle/"
+    }, {
+        name: "邮件系统",
+        url: "http://mail.bit.edu.cn/"
+    }, {
+        name: "VPN 登录",
+        url: "https://vpn.bit.edu.cn/"
+    } ]
+};
