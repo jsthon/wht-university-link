37d0e84bf03840bf9bdf1277b80d191f30f7fa81
wzdata <38850095+wzdata@users.noreply.github.com>
2018-08-13 12:36:20 +0800

    Add files via upload
    添加山东科技大学相关导航

---
+++ univ/16102.js
@@ -0,0 +1,17 @@
+var univ_list = {
+    school: "山东科技大学",
+    id: "16102",
+    link: [ {
+        name: "山科主页",
+        url: "http://www.sdust.edu.cn/"
+    }, {
+        name: "教务系统",
+        url: "http://jwgl.sdust.edu.cn/"
+    }, {
+        name: "网络教学平台",
+        url: "http://sdust.fy.chaoxing.com/portal"
+    }, {
+        name: "图书馆",
+        url: "http://lib.sdust.edu.cn/"
+    }]
+};
