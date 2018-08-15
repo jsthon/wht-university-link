1fa1b24a18d62f043666a2277cf79bcdca7084df
dix233 <36472697+dix233@users.noreply.github.com>
2018-08-15 10:32:54 +0800

    Create 2012.js
    增加上海海事大学链接

---
+++ univ/2012.js
@@ -0,0 +1,20 @@
+var univ_list = {
+    school: "上海海事",                        // 学校名称
+    id: "2012",                               // 学校 ID
+    link: [ {
+        name: "海事大学官网",                      // 链接名称少于7个汉字或其等宽字符
+        url: "http://www.shmtu.edu.cn/"   // 链接地址
+    }, 
+    {
+        name: "智慧海大门户",                      // 链接名称少于7个汉字或其等宽字符
+        url: "https://portal.shmtu.edu.cn/"   // 链接地址
+    }, 
+    {
+        name: "海大校园邮箱",                      // 链接名称少于7个汉字或其等宽字符
+        url: "https://mail.shmtu.edu.cn/"   // 链接地址
+    }, 
+    {
+        name: "海大教务处",                        // 链接数量小于或等于7个
+        url: "http://jwc.shmtu.edu.cn/"
+    } ]
+};
