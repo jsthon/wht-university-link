ce16571d1d8e5472ec17b796e4c5aa9133ef2fc9
BDZNH <pxezhsp@live.com>
2018-08-15 09:21:21 +0800

    增加西安电子科技大学

---
+++ univ/10006.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "西安电子科技大学",                        // 学校名称
+    id: "10006",                               // 学校 ID
+    link: [ {
+        name: "西电主页",                      // 链接名称少于8个汉字或其等宽字符
+        url: "https://www.xidian.edu.cn/"   // 链接地址
+    }, {
+        name: "睿思",                        // 链接数量小于或等于7个
+        url: "http://rs.xidian.edu.cn"
+    }, {
+        name: "西电开源社区",
+        url: "https://linux.xidian.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://al.lib.xidian.edu.cn"
+    }, {
+        name: "西电学堂",
+        url: "http://xt.xidian.edu.cn/"
+    }, {
+        name: "学生邮箱",
+        url: "http://stumail.xidian.edu.cn/"
+    }, {
+        name: "教务系统",
+        url: "http://jwxt.xidian.edu.cn"
+    }, ]
+};
