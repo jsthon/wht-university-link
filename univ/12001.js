c92a566be984389a155e7ea4ddd58b77b78b7bb7
KeNorizon <32296555+KeNorizon@users.noreply.github.com>
2018-08-18 18:15:26 +0800

    Create 12001.js

---
+++ univ/12001.js
@@ -0,0 +1,23 @@
+var univ_list = {
+    school: "华南理工大学",
+    id: "12001",
+    link: [ {
+        name: "华工主页",
+        url: "https://www.scut.edu.cn/"
+    }, {
+        name: "教务处",
+        url: "https://www.scut.edu.cn/academic/"
+    }, {
+        name: "图书馆",
+        url: "http://www.lib.scut.edu.cn/"
+    }, {
+        name: "统一门户",
+        url: "https://my.scut.edu.cn/"
+    }, {
+        name: "邮件系统",
+        url: "https://webmail.scut.edu.cn/scutmail/"
+    }, {
+        name: "VPN 登录",
+        url: "https://www.scut.edu.cn/vpn/"
+    } ]
+};
