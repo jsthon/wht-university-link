b62afc1096fafbfe472737568d5c42b84708a26d
ecwu <bitlabwzh@gmail.com>
2018-08-20 18:00:48 +0800

    add BNU-HKBU UIC

---
+++ univ/12798.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "北京师范大学－香港浸会大学联合国际学院",
+    id: "12798",
+    link: [ {
+        name: "UIC 主页",
+        url: "https://uic.edu.hk/en/"
+    }, {
+        name: "MIS",
+        url: "https://mis.uic.edu.hk/"
+    }, {
+        name: "iSpace",
+        url: "https://ispace.uic.edu.hk/"
+    }, {
+        name: "Webmail",
+        url: "https://webmail.uic.edu.hk/"
+    }, {
+        name: "LRC",
+        url: "http://www1.uic.edu.hk/en/lrc/"
+    }, {
+        name: "SSL",
+        url: "https://ssl.uic.edu.hk/dana-na/auth/url_default/welcome.cgi"
+    }, {
+        name: "New Portal",
+        url: "https://p.uic.edu.hk/"
+    } ]
+};
