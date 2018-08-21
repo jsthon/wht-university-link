0bf493499e01b639b25d6c2a5b1306a06548eac1
bigbro <775105396@qq.com>
2018-08-21 15:36:16 +0800

    add njupt
    添加南京邮电大学的链接

---
+++ univ/8012.js
@@ -0,0 +1,24 @@
+
+var univ_list = {
+    school: "南京邮电大学",
+    id: "8012",
+    link: [ {
+        name: "南邮主页",
+        url: "http://www.njupt.edu.cn/"
+    }, {
+        name: "本科招生网",
+        url: "http://zs.njupt.edu.cn/"
+    }, {
+        name: "研究生招生网",
+        url: "http://yzb.njupt.edu.cn/"
+    }, {
+        name: "教务管理系统",
+        url: "http://jwxt.njupt.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://lib.njupt.edu.cn/"
+    }, {
+        name: "智慧校园",
+        url: "http://ids6.njupt.edu.cn/authserver/login?service=http%3A%2F%2Fmy.njupt.edu.cn%2F"
+    } ]
+};
