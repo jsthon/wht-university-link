aa754923dc05bb2d3c5449015a2d834482718945
Jsthon <jsthon@outlook.com>
2018-09-10 20:38:32 +0800

    Create 17303.js

---
+++ univ/17303.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "厦门大学嘉庚学院",
+    id: "17303",
+    link: [ {
+        name: "嘉庚主页",
+        url: "http://www.xujc.com/"
+    }, {
+        name: "教务系统",
+        url: "http://jw.xujc.com/"
+    }, {
+        name: "教学文件系统",
+        url: "http://teach.xujc.com/"
+    }, {
+        name: "邮件系统",
+        url: "http://mail.xujc.com/extend/gb/"
+    }, {
+        name: "课表查询",
+        url: "http://jw.xujc.com/pub/index.php"
+    }, {
+        name: "体育查询",
+        url: "http://121.192.181.29:8585/EPLATE/"
+    }, {
+        name: "充值缴费",
+        url: "http://ecardservice.xmu.edu.cn:8070/"
+    } ]
+};
