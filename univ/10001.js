5360906e1fe765fb18496bbb6786d548d753fe7a
yuanjingsong <532473857@qq.com>
2018-09-14 09:28:58 +0800

    add info about XJTU

---
+++ univ/10001.js
@@ -0,0 +1,23 @@
+var univ_list = {
+    school: "西安交通大学",
+    id: "10001",
+    link: [ {
+        name: "交大主页",
+        url: "http://www.xjtu.edu.cn"
+    }, {
+        name: "教务处",
+        url: "http://dean.xjtu.edu.cn"
+    }, {
+        name: "图书馆",
+        url: "http://innopac.lib.xjtu.edu.cn/"
+    }, {
+        name: "交大邮箱",
+        url: "http://stu.xjtu.edu.cn"
+    }, {
+        name: "教务管理平台",
+        url: "http://ehall.xjtu.edu.cn/new/index.html"
+    }, {
+        name: "公共空间申请"
+        url: "https://apply.tiaozhan.com/#/"
+    }]
+};
