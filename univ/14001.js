321e1d370f55b6755df41948c09d3201721ed0e2
wangb08 <42599452+wangb08@users.noreply.github.com>
2018-09-15 20:09:06 +0800

    Update 14001.js

--- univ/14001.js
+++ univ/14001.js
@@ -8,7 +8,7 @@ var univ_list = {
         name: "研究生院",
         url: "http://ge.lzu.edu.cn/"
     }, {
-        name: "图书馆",
+        name: "兰大图书馆",
         url: "http://lib.lzu.edu.cn/"
     }, {
         name: "管理学院",
@@ -16,5 +16,14 @@ var univ_list = {
     }, {
         name: "兰大邮箱",
         url: "https://mail.lzu.edu.cn/"
-    } ]
+     }, {
+        name: "兰大教务处",
+        url: "http://jwc.lzu.edu.cn/"
+    }, {
+        name: "兰大网络电视",
+        url: "http://hdtv.lzu.edu.cn/"
+    } 
+          
+          
+          ]
 };
