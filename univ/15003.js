1abf2d906e2aa0239a122a4b675573520c37ba21
xuwangju <43360136+xuwangju@users.noreply.github.com>
2018-09-18 17:11:11 +0800

    Create 15003.js
    xuwangju

---
+++ univ/15003.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "电子科技大学",
+    id: "15003",
+    link: [ {
+        name: "电子科大主页",
+        url: "http://www.uestc.edu.cn/"
+    },  {
+        name: "电子学院主页",
+        url: "http://bbs.uestc.edu.cn/"
+    },{
+        name: "研究生院主页",
+        url: "http://gr.uestc.edu.cn/"
+    }, {
+{
+        name: "信息门户系统",
+        url: "http://portal.uestc.edu.cn/"
+    },  {
+        name: "研究生系统",
+        url: "http://yjsjy.uestc.edu.cn/pyxx/home/index"
+    },        name: "清水河畔",
+        url: "http://bbs.uestc.edu.cn/"
+    }, {
+        name: "电子科大研招网",
+        url: "http://yz.uestc.edu.cn/"
+    } ]
+};
