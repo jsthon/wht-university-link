c5c8a4beaf13fc680872e046e6a64b1c134cf167
Jsthon <jsthon@outlook.com>
2018-09-18 22:23:33 +0800

    fix 15003.js

--- univ/15003.js
+++ univ/15003.js
@@ -4,20 +4,20 @@ var univ_list = {
     link: [ {
         name: "电子科大主页",
         url: "http://www.uestc.edu.cn/"
-    },  {
+    }, {
         name: "电子学院主页",
         url: "http://bbs.uestc.edu.cn/"
-    },{
+    }, {
         name: "研究生院主页",
         url: "http://gr.uestc.edu.cn/"
     }, {
-{
         name: "信息门户系统",
         url: "http://portal.uestc.edu.cn/"
-    },  {
+    }, {
         name: "研究生系统",
         url: "http://yjsjy.uestc.edu.cn/pyxx/home/index"
-    },        name: "清水河畔",
+    }, {
+        name: "清水河畔",
         url: "http://bbs.uestc.edu.cn/"
     }, {
         name: "电子科大研招网",
