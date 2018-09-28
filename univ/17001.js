4ff9e0ce4683c0ccf72399de9397204384ec677a
yanlongxiang <z00823823@126.com>
2018-09-28 22:11:53 +0800

    Create 17001.js
    厦门大学

---
+++ univ/17001.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "厦门大学",
+    id: "17001",
+    link: [ {
+        name: "厦大主页",
+        url: "https://www.xmu.edu.cn"
+    }, {
+        name: "教务处",
+        url: "http://jwc.xmu.edu.cn"
+    }, {
+        name: "信息与网络中心",
+        url: "https://net.xmu.edu.cn"
+    }, {
+        name: "信息门户",
+        url: "https://i.xmu.edu.cn"
+    }, {
+        name: "知识资源港",
+        url: "http://library.xmu.edu.cn"
+    }, {
+        name: "易班",
+        url: "http://www.yiban.cn"
+    }, {
+        name: "厦大网址导航",
+        url: "http://123.xmu.edu.cn"
+    } ]
+};
