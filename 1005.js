c5b9a10b56d547c432481542a1b46fc2c7aa92c2
fecozn <fecozn@gmail.com>
2018-10-07 11:06:25 +0800

    Create 1005.js

---
+++ 1005.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "北京邮电大学",                        // 学校名称
+    id: "1005",                               // 学校 ID
+    link: [ {
+        name: "北邮主页",                      // 链接名称少于7个汉字或其等宽字符
+        url: "https://www.bupt.edu.cn/"   // 链接地址
+    }, {
+        name: "教务系统",
+        url: "https://jwxt.bupt.edu.cn/" 
+    }, {
+        name: "北邮IPTV",
+        url: "http://tv.byr.cn/show"
+    }, {
+        name: "信息门户",
+        url: "https://vpn.bupt.edu.cn/http/my.bupt.edu.cn/index.portal"
+    }, {
+        name: "电子邮箱",
+        url: "https/mail.bupt.edu.cn/"
+    }, {
+        name: "图书馆主页",
+        url: "https://lib.bupt.edu.cn/index.html"
+    }, {
+        name: "北邮人论坛",                        // 链接数量小于或等于7个
+        url: "http://bbs.byr.cn/"
+    } ]
+};
