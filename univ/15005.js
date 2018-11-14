128c07a4f204347a896c105645f6247a78c0d549
Vietronic <wllkn@qq.com>
2018-11-14 16:20:09 +0800

    Create 15005.js
    add CDUT links

---
+++ univ/15005.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "成都理工大学",
+    id: "15005",
+    link: [ {
+        name: "成理主页",
+        url: "http://www.cdut.edu.cn/"
+    }, {
+        name: "成理教务处",
+        url: "http://www.aao.cdut.edu.cn/"
+    }, {
+        name: "成理图书馆",
+        url: "http://www.lib.cdut.edu.cn/"
+    }, {
+        name: "成理计财处",
+        url: "http://www.campuscard.cdut.edu.cn/"
+    }, {
+        name: "信息技术中心",
+        url: "http://www.cdut.edu.cn/xxzx/"
+    }, {
+        name: "校内正版软件",
+        url: "http://202.115.128.21/msinstall/instal.html"
+    }, {
+        name: "校内VPN",
+        url: "http://www.cdut.edu.cn/xxzx/news/141768416948456210.html"
+    } ]
+};
