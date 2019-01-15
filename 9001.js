12a6b8196643536ac5691141cae38aaf577a5c64
AugustLeoQiang <42860720+AugustLeoQiang@users.noreply.github.com>
2019-01-15 13:15:10 +0800

    Create 9001.js

---
+++ 9001.js
@@ -0,0 +1,20 @@
+var univ_list = {
+    school: "浙江大学",
+    id: "9001",
+    link: [ {
+        name: "浙大主页",
+        url: "http://www.zju.edu.cn/"
+    }, {
+        name: "信息系统",
+        url: "http://zuits.zju.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://libweb.zju.edu.cn/libweb/"
+    }, {
+        name: "浙大邮箱",
+        url: "https://mail.zju.edu.cn/"
+    }, {
+        name: "浙大研究生院",
+        url: "http://grs.zju.edu.cn/"
+    }]
+};
