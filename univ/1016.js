1dcd948a704bdb98aa06edbaa48e4148d058c25d
XIAO Yu <yu.xiao.fr@gmail.com>
2019-04-17 21:22:52 +0800

    北京工业大学

---
+++ univ/1016.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "北京工业大学",
+    id: "1016",
+    link: [ {
+        name: "工大主页",
+        url: "http://www.bjut.edu.cn/"
+    }, {
+        name: "教务系统",
+        url: "http://gdjwgl.bjut.edu.cn"
+    }, {
+        name: "内网综合",
+        url: "https://my.bjut.edu.cn"
+    }, {
+        name: "校历查询",
+        url: "https://my.bjut.edu.cn/group/undergraduate/schoolcal"
+    }, {
+        name: "IPV6网关",
+        url: "https://lgn6.bjut.edu.cn/"
+    }, {
+        name: "学工系统",
+        url: "http://xgxt.bjut.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://lib.bjut.edu.cn/"
+    } ]
+};
