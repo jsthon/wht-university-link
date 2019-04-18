99141d0774996f4cbae0e402d455b2b8077f8f16
SailFlorve <xdygxh@163.com>
2019-04-18 17:51:01 +0800

    Create 9004.js

---
+++ univ/9004.js
@@ -0,0 +1,45 @@
+var univ_list = {
+
+    school: "杭州电子科大",
+
+    id: "9004",
+
+    link: [ {
+
+        name: "杭电主页",
+
+        url: "https://www.hdu.edu.cn/"
+
+    }, {
+
+        name: "数字杭电",
+
+        url: "https://i.hdu.edu.cn/"
+
+    }, {
+
+        name: "图书馆",
+
+        url: "http://www.lib.hdu.edu.cn/"
+
+    }, {
+
+        name: "选课系统",
+
+        url: "http://jxgl.hdu.edu.cn/"
+
+    }, {
+
+        name: "教务处",
+
+        url: "http://jwc.hdu.edu.cn/"
+
+    }, {
+
+        name: "杭电邮箱",
+
+        url: "https://mail.hdu.edu.cn/"
+
+    }]
+
+};
