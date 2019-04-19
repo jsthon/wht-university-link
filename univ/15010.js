91a6af2dcbb30710e2efe3c21a0851eb0183301b
Mystery0 <mystery0dyl520@gmail.com>
2019-04-19 12:22:01 +0800

    feat: 添加西华大学

---
+++ univ/15010.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "西华大学",
+    id: "15010",
+    link: [ {
+        name: "西华大学官网",
+        url: "http://www.xhu.edu.cn/"
+    }, {
+        name: "教务系统",
+        url: "http://jwc.xhu.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://www.lib.xhu.edu.cn/"
+    }, {
+        name: "邮件系统",
+        url: "http://mail.xhu.edu.cn/"
+    }, {
+        name: "信息门户",
+        url: "http://urp.xhu.edu.cn/"
+    }, {
+        name: "课程中心",
+        url: "http://kczx.xhu.edu.cn/"
+    }, {
+        name: "西瓜课表",
+        url: "https://coolapk.com/apk/com.weilylab.xhuschedule"
+    } ]
+};
