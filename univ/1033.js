c799af64d02945fa9898aa030b534a27c261754f
Aoerz <aoerz@qq.com>
2019-04-20 14:30:42 +0800

    Create 1033.js

---
+++ univ/1033.js
@@ -0,0 +1,23 @@
+var univ_list = {
+    school: "华北电力大学（北京）",                        
+    id: "1033",                               
+    link: [ {
+        name: "华电主页",  
+        url: "http://www.ncepu.edu.cn/"   
+    }, {
+        name: "教务处",
+        url: "http://jwc.ncepu.edu.cn/"
+    }, {
+        name: "邮电系统",                        
+        url: "http://mail.ncepu.edu.cn/"
+    }, {
+        name: "服务平台",
+        url: "http://my.ncepu.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://library.ncepu.edu.cn/"
+    }, {
+        name: "webVPN",
+        url: "https://webvpn.ncepu.edu.cn/"
+    } ]
+};
