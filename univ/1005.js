22cc6761fed9049765c96cacfd5356e73d4d2e69
Sandothers <fecozn@gmail.com>
2019-05-06 21:11:19 +0800

    Update 1005.js

--- univ/1005.js
+++ univ/1005.js
@@ -2,8 +2,8 @@ var univ_list = {
     school: "北京邮电大学",
     id: "1005",
     link: [{
-        name: "在校生导航",
-        url: "https://www.bupt.edu.cn/content/content.php?p=86_24_2297/"
+        name: "北邮首页",
+        url: "https://www.bupt.edu.cn/"
     }, {
         name: "教务系统",
         url: "https://jwxt.bupt.edu.cn/"
