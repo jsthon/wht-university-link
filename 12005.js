b6d8dc148a7d85adcf971fd44803b676d9b977a1
Ne0 <open19981122@gmail.com>
2019-05-17 15:58:11 +0800

    添加广东工业大学信息

---
+++ 12005.js
@@ -0,0 +1,23 @@
+var univ_list = {
+    school: "广东工业大学",
+    id: "12005",
+    link: [ {
+        name: "工大首页",
+        url: "http://www.gdut.edu.cn/"
+    }, {
+        name: "教务系统",
+        url: "http://jxfw.gdut.edu.cn/"
+    }, {
+        name: "新闻通知网",
+        url: "http://news.gdut.edu.cn/"
+    }, {
+        name: "工大学生邮箱",
+        url: "http://mail.mail2.gdut.edu.cn/"
+    }, {
+        name: "网上办事大厅",
+        url: "http://e.gdut.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://library.gdut.edu.cn/"
+    } ]
+};
