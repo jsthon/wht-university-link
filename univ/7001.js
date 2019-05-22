7cb5fb0d49053a680b9fe8aacf20a49f2ddbab7d
jocelyn <43110402+jocelynzym@users.noreply.github.com>
2019-05-22 10:02:03 +0800

    add USTC
    中国科大

---
+++ univ/7001.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "清华大学",
+    id: "1001",
+    link: [ {
+        name: "科大主页",
+        url: "https://www.ustc.edu.cn/"
+    }, {
+        name: "教务系统",
+        url: "https://jw.ustc.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://lib.ustc.edu.cn/"
+    }, {
+        name: "科大邮箱",
+        url: "http://mail.ustc.edu.cn/"
+    }, {
+        name: "评课社区",
+        url: "https://www.icourse.club/"
+    }, {
+        name: "开源镜像",
+        url: "http://mirrors.ustc.edu.cn/"
+    }, {
+        name: "bbs",
+        url: "http://bbs.ustc.edu.cn/"
+    } ]
+};
