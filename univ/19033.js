7754cb774d476a990b04cbcd2e9dc1e85f96d397
seceretrui <1428433559@qq.com>
2019-09-26 11:59:04 +0800

    update

---
+++ univ/19033.js
@@ -0,0 +1,23 @@
+var univ_list = {
+    school: "重庆工程学院",
+    id: "17001",
+    link: [ {
+        name: "重工主页",
+        url: "http://www.cqie.edu.cn/Index.shtml"
+    }, {
+        name: "教务处",
+        url: "http://jw.cqie.edu.cn/"
+    }, {
+        name: "网络教学平台",
+        url: "http://wj.cqie.edu.cn/meol/homepage/common/"
+    }, {
+        name: "学生综合管理系统",
+        url: "http://xs.cqie.edu.cn/"
+    }, {
+        name: "图书管藏",
+        url: "http://lib.cqie.edu.cn/cmsController.do?goPage&page=index&siteCode=portal"
+    }, {
+        name: "就业管理",
+        url: "http://192.168.0.26/"
+    } ]
+};
