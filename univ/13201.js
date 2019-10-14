c93e1ab813d1bfc7e2ec59fdb289c38562e2118e
Xing <Virgo_2333@163.com>
2019-10-14 18:49:16 +0800

    修复工大导航和教务系统地址

--- univ/13201.js
+++ univ/13201.js
@@ -3,21 +3,21 @@ var univ_list = {
     id: "13201",
     link: [ {
         name: "工大导航（内网）",
-        url: "http://172.16.10.210//"
+        url: "http://172.16.10.210"
     },,{
         name: "湖工大教务系统",
-        url: "http://218.75.197.123:83//"
+        url: "http://218.75.197.123:83"
     },{
         name: "湖工大主页",
-        url: "http://www.hut.edu.cn/"
+        url: "http://www.hut.edu.cn"
     },{
         name: "图书馆",
-        url: "http://218.75.197.122:8090/"
+        url: "http://218.75.197.122:8090"
     }, {
         name: "学生邮箱",
-        url: "http://mail.stu.hut.edu.cn/"
+        url: "http://mail.stu.hut.edu.cn"
     }, {
         name: "工大助手",
-        url: "http://huthelper.cn/"
+        url: "http://huthelper.cn"
     } ]
 };
