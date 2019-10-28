625cc79da82e128a1be576174b421f0021007321
xiaoyan94 <ruguo_0904@163.com>
2019-10-28 19:05:59 +0800

    Create 16103.js
    QUST & PT

---
+++ 16103.js
@@ -0,0 +1,23 @@
+var univ_list = {
+    school: "青岛科大",                        // 学校名称
+    id: "16103",                               // 学校 ID
+    link: [ {
+        name: "科大主页",                      // 链接名称少于7个汉字或其等宽字符
+        url: "https://www.qust.edu.cn/"   // 链接地址
+    }, {
+        name: "智慧青科",                      // 链接名称少于7个汉字或其等宽字符
+        url: "https://i.qust.edu.cn/"   // 链接地址
+    }, {
+        name: "学生邮箱",                        // 链接数量小于或等于7个
+        url: "https://mails.qust.edu.cn/"
+    }, {
+        name: "北邮人PT",                        // 链接数量小于或等于7个
+        url: "https://bt.byr.cn/index.php"
+    }, {
+        name: "蒲公英PT",                        // 链接数量小于或等于7个
+        url: "https://npupt.com/index.php"
+    }, {
+        name: "皇后PT",                        // 链接数量小于或等于7个
+        url: "https://open.cd/"
+    } ]
+};
