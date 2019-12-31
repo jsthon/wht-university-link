3a3bdef51652d544bdef1ce08620b199fb39dd09
Tifinity <1139390530@qq.com>
2019-12-31 11:24:45 +0800

    增加中山大学

---
+++ univ/12002.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "中山大学",
+    id: "12002",
+    link: [ {
+        name: "主页",
+        url: "https://www.sysu.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://library.sysu.edu.cn/"
+    }, {
+        name: "教务系统",
+        url: "https://jwxt.sysu.edu.cn/"
+    }, {
+        name: "我的中大",
+        url: "http://my.sysu.edu.cn/"
+    }, {
+        name: "统一门户",
+        url: "https://portal.sysu.edu.cn/"
+    }, {
+        name: "帮助台",
+        url: "https://helpdesk.sysu.edu.cn"
+    }, {
+        name: "服务中心",
+        url: "http://usc.sysu.edu.cn/zs_portal/index"
+    } ]
+};
