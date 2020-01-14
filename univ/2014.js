aad172dbf460dd4696b5dfbfc448396638707564
jocelyn <43110402+jocelynzym@users.noreply.github.com>
2020-01-14 10:27:39 +0800

    Create 2014.js

---
+++ univ/2014.js
@@ -0,0 +1,17 @@
+var univ_list = {
+    school: "上海海洋",
+    id: "2014",
+    link: [ {
+        name: "海大主页",
+        url: "https://www.shou.edu.cn/"
+    }, {
+        name: "海大邮箱",
+        url: "https://mail.shou.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://library.shou.edu.cn/705/list.htm"
+    }, {
+        name: "bbs",
+        url: "http://shou.myubbs.com/forum.php"
+    }]
+};
