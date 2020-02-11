bc6ce8a44d1dc0624fdb245902c047506a01e4d0
linjuanmao <53474973+linjuanmao@users.noreply.github.com>
2020-02-11 15:30:11 +0800

    Create 12004.js

---
+++ univ/12004.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "华南师范大学",
+    id: "12004",
+    link: [ {
+        name: "正方教务管理",
+        url: "https://jwc.scnu.edu.cn/"
+    }, {
+        name: "教学信息系统",
+        url: "https://jwxt.scnu.edu.cn/xtgl/"
+    }, {
+        name: "图书馆",
+        url: "http://lib.scnu.edu.cn/"
+    }, {
+        name: "十校公选",
+        url: "https://unitown.scnu.edu.cn/"
+    }, {
+        name: "非正式课程",
+        url: "https://fzskc.scnu.edu.cn/"
+    }, {
+        name: "砺儒云课堂",
+        url: "http://moodle.scnu.edu.cn/"
+    }, {
+        name: "学生服务平台",
+        url: "https://ssp.scnu.edu.cn/"
+    } ]
+};
