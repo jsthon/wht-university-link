c15964916da34a412f4169ce55c651d0924a1484
Loaker-B <61631455+Loaker-B@users.noreply.github.com>
2020-03-01 02:23:46 +0800

    Create 5013.js
    大连外国语大学

---
+++ univ/5013.js
@@ -0,0 +1,26 @@
+  var univ_list = {
+    school: "大连外国语大学",
+    id: "5013",
+    link: [ {
+        name: "DLUFL",
+        url: "http://www.dlufl.edu.cn/"
+    }, {
+        name: "数字大外",
+        url: "http://i.dlufl.edu.cn/"
+    }, {
+        name: "教务系统",
+        url: "http://jwgl.jiaowu.dlufl.edu.cn/jxjsxsd/framework/xsMain.jsp"
+    }, {
+        name: "VOD系统",
+        url: "http://vod.dlufl.edu.cn/"
+    }, {
+        name: "大外云盘",
+        url: "https://pan.dlufl.edu.cn/"
+    }, {
+        name: "一卡通系统",
+        url: "http://ecard.dlufl.edu.cn/sso_jump.jsp"
+    }, {
+        name: "网络中心",
+        url: "http://net.dlufl.edu.cn/"
+    } ]
+};
