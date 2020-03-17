2979d56d42be7dd6e834ff91561dc9db8c0ac3c8
WTA1999 <62271785+WTA1999@users.noreply.github.com>
2020-03-17 10:35:00 +0800

    Create 15667.js

---
+++ 大学/15667.js
@@ -0,0 +1,25 @@
+var univ_list = {
+    school: "西南财经大学天府学院",                        // 学校名称
+    id: "15667",                               // 学校 ID
+    link: [{
+        name: "门户",                        // 链接数量小于或等于7个
+        url: "http://portal.tfswufe.edu.cn/web/guest/243/"
+    }, {
+        name: "邮箱",                      // 链接名称少于7个汉字或其等宽字符
+        url: "http://mail.stu.tfswufe.edu.cn/"   // 链接地址
+    },{
+        name: "教务",                        // 链接数量小于或等于7个
+        url: "http://jwc.tfswufe.edu.cn/eams/login.action"
+    },{
+        name: "校历",                      // 链接名称少于7个汉字或其等宽字符
+        url: "https://www.tfswufe.edu.cn/xxbgs/tfxl.htm"   // 链接地址
+    },{
+        name: "Spoc",                        // 链接数量小于或等于7个
+        url: "https://spoc.tfswufe.edu.cn/"
+    },{
+        name: "云平台",                      // 链接名称少于7个汉字或其等宽字符
+        url: "https://mtc.tfswufe.edu.cn/tfcloud/"   // 链接地址
+    },{
+        name: "就业指导",                        // 链接数量小于或等于7个
+        url: "http://jy.tfswufe.edu.cn/"
+    },]
