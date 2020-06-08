22292cff12bbf6c18e4e22575c80e0f870434643
7ucifer <38828377+7ucifer66@users.noreply.github.com>
2020-06-08 18:42:28 +0800

    添加电子科技大学中山学院

---
+++ univ 大学/12703.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "电子科技大学中山学院",
+    id: "12703",
+    link: [ {
+      name: "中山学院主页",
+      url: "https://www.zsc.edu.cn/"
+    }, {
+      name: "图书馆",
+      url: "http://lib.zsc.edu.cn/"
+    }, {
+      name: "教务处",
+      url: "http://jwc.zsc.edu.cn/"
+    }, {
+      name: "课程平台",
+      url: "http://zsc.fanya.chaoxing.com/"
+    }, {
+      name: "教务系统",
+      url: "http://jwgln.zsc.edu.cn/jsxsd/"
+    }, {
+      name: "素拓系统",
+      url: "http://210.38.224.229/suzhi/"
+    }, {
+      name: "门户系统",
+      url: "https://cas.zsc.edu.cn/"
+    } ]
+};
