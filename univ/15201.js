bdaed5606f397500d8e932ceb7972a283be0cbae
SongXin1015 <64391901+SongXin1015@users.noreply.github.com>
2020-06-21 07:37:55 +0800

    create nsmc

---
+++ univ/15201.js
@@ -0,0 +1,16 @@
+var univ_list = {
+    school: "川北医学院",                        // 学校名称
+    id: "15201",                               // 学校 ID
+    link: [ {
+        name: "学校官网",                      // 链接名称少于7个汉字或其等宽字符
+        url: "https://www.nsmc.edu.cn/"   // 链接地址
+    }, 
+    {
+        name: "图书馆",                        // 链接数量小于或等于7个
+        url: "http://lib.nsmc.edu.cn/"
+    }
+    {
+        name: "教务系统",                        // 链接数量小于或等于7个
+        url: "https://www.nsmc.edu.cn/zh/21504.html"
+    }]
+};
