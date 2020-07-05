69b21333bf8bde96ae44ae8e18964264eafc11e7
Chiang Weikuang <62584929+bitepen@users.noreply.github.com>
2020-07-05 11:54:47 +0800

    Create 8005.js

---
+++ 8005.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "东南大学",
+    id: "8005",
+    link: [ {
+        name: "东大主页",
+        url: "https://www.seu.edu.cn/"
+    }, {
+        name: "教务处",
+        url: "https://jwc.seu.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://www.lib.seu.edu.cn/"
+    }, {
+        name: "邮件系统",
+        url: "https://mail.seu.edu.cn/"
+    }, {
+        name: "东大云盘",
+        url: "https://pan.seu.edu.cn/"
+    }, {
+        name: "办事大厅",
+        url: "http://ehall.seu.edu.cn/"
+    }, {
+        name: "健康申报",
+        url: "http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/index.do#/dailyReport"
+    } ]
+};
