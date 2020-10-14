e21fa63f01a15cec1917f20ba68d4c4f24687c7a
Carsonn-Chen <61925840+Carsonn-Chen@users.noreply.github.com>
2020-10-14 16:05:59 +0800

    Add files via upload

---
+++ univ/11003.js
@@ -0,0 +1,33 @@
+{
+        id: 11003,
+        name: "华中农大"
+    },
+
+
+
+var univ_list = {
+    school: "华中农大",
+    id: "11003",
+    link: [ {
+        name: "华农主页",
+        url: "http://www.hzau.edu.cn/"
+    }, {
+        name: "教务系统",
+        url: "http://jwgl.hzau.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://lib.hzau.edu.cn/"
+    }, {
+        name: "学生邮箱",
+        url: "https://webmail.hzau.edu.cn/"
+    }, {
+        name: "信息学院itc",
+        url: "http://itc.hzau.edu.cn/"
+    }, {
+        name: "团委会",
+        url: "http://tw.hzau.edu.cn/"
+    }, {
+        name: "本科生院",
+        url: "http://bksy.hzau.edu.cn/"
+    } ]
+};
