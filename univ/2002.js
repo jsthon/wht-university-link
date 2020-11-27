68bfa860348a9ed60257ae5beee8811d6e016436
Liz&Geiz <Liz-And-Geiz@outlook.com>
2020-11-27 16:39:10 +0800

    create 2002.js

---
+++ univ/2002.js
@@ -0,0 +1,20 @@
+var univ_list = {
+    school: "上海交通大学",                        
+    id: "2002",                               
+    link: [ {
+        name: "交大主页",  
+        url: "https://www.sjtu.edu.cn/"   
+    }, {
+        name: "教务处",
+        url: "http://www.jwc.sjtu.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://www.lib.sjtu.edu.cn/f/main/index.shtml"
+    }, {    
+        name: "邮件系统",                        
+        url: "http://mail.sjtu.edu.cn/"
+    }, {
+        name: "My SJTU",
+        url: "http://my.sjtu.edu.cn/"
+    }]
+};
