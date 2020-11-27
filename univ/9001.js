a73ce67f6ee9a82f77338376231ca7f23d5bb289
Liz&Geiz <Liz-And-Geiz@outlook.com>
2020-11-27 17:44:24 +0800

    create 8001.js create 9001.js

---
+++ univ/9001.js
@@ -0,0 +1,20 @@
+var univ_list = {
+    school: "浙江大学",
+    id: "9001",
+    link: [ {
+        name: "浙大主页",
+        url: "http://www.zju.edu.cn/"
+    }, {
+        name: "WebVPN",
+        url: "https://webvpn.zju.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "http://libweb.zju.edu.cn/"
+    }, {
+        name: "电子邮箱",
+        url: "http://mail.zju.edu.cn/"
+    }, {
+        name: "信息技术中心",
+        url: "http://zuits.zju.edu.cn/"
+    } ]
+};
