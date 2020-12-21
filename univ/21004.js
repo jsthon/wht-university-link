86fc7fba73953f439bc579a066822161319521d9
星空梦想zhang <57982333+95579580@users.noreply.github.com>
2020-12-21 18:10:18 +0800

    21004.js
    石家庄铁道大学

---
+++ univ/21004.js
@@ -0,0 +1,13 @@
+var univ_list = {
+    school: "石家庄铁道大学",                        // 学校名称
+    id: "21004",                               // 学校 ID
+    link: [ {
+        name: "铁大主页",                      // 链接名称少于7个汉字或其等宽字符
+        url: "http://www.stdu.edu.cn/"   // 链接地址
+    }, { name: "教务处",                      // 链接名称少于7个汉字或其等宽字符
+        url: "http://tiedao.vatuu.com/index.html"  
+    }, {
+        name: "图书馆",                        // 链接数量小于或等于7个
+        url: "http://www.stdu.edu.cn/"
+    } ]
+};
