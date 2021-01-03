bc6a9bcabcfd0a7d8f08c3c8a7cd52c2ee060477
“jmzdd” <2041418454@qq.com>
2021-01-03 14:03:20 +0800

    master

---
+++ univ/17426.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "黎明职业大学",                        // 学校名称
+    id: "17426",                               // 学校 ID
+    link: [ {
+        name: "黎大官网",                      // 链接名称少于7个汉字或其等宽字符
+        url: "http://www.lmu.cn/"   // 链接地址
+    }, {
+        name: "黎大图书馆",                        // 链接数量小于或等于7个
+        url: "http://lib.lmu.edu.cn/"
+    }, {
+        name: "黎大教务处",
+        url: "http://jwc.lmu.edu.cn/"
+    }, { 
+        name: "黎大邮箱系统",
+        url: "http://mail.lmu.cn/"
+    }, { 
+        name: "黎大地图",
+        url: "https://720yun.com/t/58e24j85xyw?scene_id=8948125"
+    }, {
+        name: "教务管理系统",
+        url: "jw.lmu.cn"
+    }, {
+        name: "就业信息网",
+        url: "http://jyzd.lmu.edu.cn/"  
+    } ]
+};
