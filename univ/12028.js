ff601fb3a3c09bafc29ca686033b58994de6e2d1
mirei <3100496917@qq.com>
2021-08-11 20:46:19 +0800

    create 12028.js

---
+++ univ/12028.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "哈尔滨工业大学深圳研究生院",
+    id: "12028",
+    link: [ {
+        name: "工大主页",
+        url: "https://www.hitsz.edu.cn/index.html"
+    }, {
+        name: "教务系统",
+        url: "http://jw.hitsz.edu.cn/"
+    }, {
+        name: "图书馆",
+        url: "https://lib.utsz.edu.cn/index.html"
+    }, {
+        name: "校园邮箱",
+        url: "https://mail.hit.edu.cn/"
+    }, {
+        name: "VPN",
+        url: "https://vpn.hitsz.edu.cn/"
+    }, {
+        name: "课程回放",
+        url: "http://219.223.238.14:88/ve/"
+    }, {
+        name: "校园网登录",
+        url: "http://10.248.98.2/"
+    } ]
+};
