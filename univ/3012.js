ed930d2ef933153284b6910c01e611ad3805fb48
Haley Ehrich <haley_ehrich@outlook.com>
2021-08-25 22:07:20 +0800

    create 3012.js
    添加黑龙江大学信息

---
+++ univ/3012.js
@@ -0,0 +1,14 @@
+var univ_list = {
+  school: "黑龙江大学",
+  id: "3012",
+  link: [ {
+    name: "黑大主页",
+    url: "http://www.hlju.edu.cn/"
+  },{
+    name: "黑大应用门户",
+    url: "http://light.hlju.edu.cn/_web/_apps/default.html"
+  },{
+    name: "黑大选课系统",
+    url: "http://xsxk.hlju.edu.cn/xsxk/logout.xk"
+  } ]
+};
