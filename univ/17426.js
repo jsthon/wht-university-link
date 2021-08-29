dbe3e96300990262ab6981f12f1fd6f10aacbd71
“jmzdd” <2041418454@qq.com>
2021-08-29 11:27:06 +0800

    更新黎明职业大学校园导航

--- univ/17426.js
+++ univ/17426.js
@@ -3,24 +3,24 @@ var univ_list = {
     id: "17426",                               // 学校 ID
     link: [ {
         name: "黎大官网",                      // 链接名称少于7个汉字或其等宽字符
-        url: "http://www.lmu.cn/"   // 链接地址
+        url: "https://www.lmu.cn/"   // 链接地址
     }, {
         name: "黎大图书馆",                        // 链接数量小于或等于7个
-        url: "http://lib.lmu.edu.cn/"
+        url: "https://www.lmu.edu.cn/tsg/index.htm"
     }, {
         name: "黎大教务处",
-        url: "http://jwc.lmu.edu.cn/"
-    }, { 
-        name: "黎大邮箱系统",
-        url: "http://mail.lmu.cn/"
+        url: "https://www.lmu.edu.cn/jwc/index.htm"
     }, { 
         name: "黎大地图",
         url: "https://720yun.com/t/58e24j85xyw?scene_id=8948125"
+    }, { 
+        name: "网上办事大厅",
+        url: "http://mail.lmu.cn/"
     }, {
-        name: "教务管理系统",
-        url: "jw.lmu.cn"
+        name: "教学管理系统",
+        url: "http://jw.lmu.cn/admin/login#"
     }, {
-        name: "就业信息网",
-        url: "http://jyzd.lmu.edu.cn/"  
+        name: "就业创业指导中心",
+        url: "https://www.lmu.edu.cn/jycyzdzx/index.htm"  
     } ]
 };
