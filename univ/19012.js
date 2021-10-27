c5f1616b9416620fd02e8afd4252e58957f20640
NoHeartPen <NoHeartPen@outlook.com>
2021-10-27 09:18:44 +0800

    Create SISU

---
+++ univ/19012.js
@@ -0,0 +1,21 @@
+var univ_list = {
+    school: "四川外国语大学",                       
+    id: "19012",                              
+    link: [ {
+        name: "川外主页",                     
+        url: "http://www.sisu.edu.cn/"   
+    }, {
+        name: "办事大厅",                        
+        url: "http://ehall.sisu.edu.cn/new/index.html"
+    },{
+        name: "数字资源",                        
+        url: "http://www.sisu.edu.cn/szzy.htm"
+    },{
+        name:"学习通",
+        url:"http://sisu.fanya.chaoxing.com/portal"
+    },{
+        name:"图书馆",
+        url:"http://lib.sisu.edu.cn/"
+    }
+	]
+};
